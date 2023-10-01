6098ff821c825cc89d2403c89eaaaf8d6bc58784
Fundile <ftshungu@gmail.com>
2023-10-01 09:08:07 -0700

    My first commit

--- 0x00-shell_basics/create_directory.sh
+++ 0x00-shell_basics/create_directory.sh
@@ -3,4 +3,7 @@ target_directory="/alx-system_engineering-devops/0x00-shell_basics/tmp"
 new_directory_name="my_first_directory"
 if [ -d "$target_directory" ]; then
     mkdir "$target_directory/$new_directory_name"
+    echo "Directory '$new_directory_name' created within '$target_directory'."
+else
+    echo "Target directory '$target_directory' does not exist."
 fi
