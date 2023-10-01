686ea76f4cc1f6d93cc40ccf07beb1cd9adab238
Fundile <ftshungu@gmail.com>
2023-09-30 17:32:48 -0700

    My first commit

--- 0x00-shell_basics/create_directory.sh
+++ 0x00-shell_basics/create_directory.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-/alx-system_engineering-devops/0x00-shell_basics/tmp; mkdir my_first_directory
+target_directory="/alx-system_engineering-devops/0x00-shell_basics/tmp"
+new_directory_name="my_first_directory"
+if [ -d "$target_directory" ]; then
+    mkdir "$target_directory/$new_directory_name"
+fi
